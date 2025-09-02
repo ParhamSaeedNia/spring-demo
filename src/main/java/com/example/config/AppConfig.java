0354f9753f7eabc5af1a3daa91617490bd765d9a
ParhamSaeednia <saeedniaparham@gmail.com>
2025-09-02 09:39:52 +0330

    init

---
+++ src/main/java/com/example/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.example.config;
+
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan(basePackages = "com.example")  // tells Spring where to look
+public class AppConfig {
+}
